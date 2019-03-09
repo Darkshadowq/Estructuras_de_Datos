75d5c5db4a654782e3b15db749e8c6dab8e44dad
usuario pc <usuario pc@10.10.6.122>
2019-03-09 10:04:20 -0500

    meh, de manera muy simple tiendo a no pagar extras y para cuando el profe dijo que era obligatorio esto ya era tarde pero preferible algo que nada

---
+++ src/objetos.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+import org.xml.sax.InputSource;
+
+public class objetos {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int nrutasm,nrutast,tiemporutasm,tiemporutast,d,p,hp;
+	    
+	
+ }
+}
+
+	
+
+
+	
+
